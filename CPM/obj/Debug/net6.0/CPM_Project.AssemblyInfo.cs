ede9c800199dc1f50036e945ef5870863289cfb9
chanox <peace.banget@gmail.com>
2024-10-08 09:57:25 +0800

    Update

--- CPM/obj/Debug/net6.0/CPM_Project.AssemblyInfo.cs
+++ CPM/obj/Debug/net6.0/CPM_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPM_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e625f99533143d1775d1df7fb6e654e9589c3b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPM_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPM_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
